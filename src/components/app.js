5e3a224530e4fb5242674503fb686ec58f874bc2
Marck <mercercountynj@gmail.com>
2016-05-01 19:04:30 -0400

    todos-list component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import TodosList from './todos-list';
+
+
+const todos = [
+{
+     task: 'Learn React JS',
+     isCompleted: false
+},
+{
+     task: 'Learn Webpack + ES6',
+     isCompleted: true
+}
+];
+
 
 export default class App extends React.Component {
      render () {
           return (
                <div>
                     <h1>React ToDo App</h1>
+                    <TodosList />
                </div>
           )
      }
